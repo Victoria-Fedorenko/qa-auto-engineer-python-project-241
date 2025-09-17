cd73a96138ebaa64283e0b48a01baaacbb5af48e
Victoria-Fedorenko <vnfedorenko97@gmail.com>
2025-09-17 22:30:41 +0300

    added json files, added read_files function

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,18 @@
 import argparse
+import json
+from path import Path
+
+FILE1 = Path('..', '..', 'file1.json')
+FILE2 = Path('..', '..', 'file2.json')
+
+def read_files(file1=FILE1, file2=FILE1):
+
+    config_file_1 = json.load(file1)
+    config_file_2 = json.load(file2)
+
+    return config_flie_1, config_file_2
+
+
 
 def main():
 
@@ -13,6 +27,8 @@ def main():
     parser.add_argument('-f', '--format')
 
     parser.parse_args()
+
+    config_file_1, config_file_2 = read_files()
 
 if __name__ == '__main__':
     main()
