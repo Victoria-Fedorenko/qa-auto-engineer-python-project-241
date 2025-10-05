97c1e5d27d24a926aa7b043cd088a5f6383d09a2
Victoria-Fedorenko <vnfedorenko97@gmail.com>
2025-10-05 12:55:08 +0300

    now read_files function can read yml files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import yaml
 from pathlib import Path
 
 
@@ -7,14 +8,25 @@ def read_files(file1, file2):
 
     def read_file(file_path_str):
         file_path = Path(file_path_str)
-        with open(file_path, 'r') as my_file:
-            try:
-                my_data = json.load(my_file)
-                return my_data
-            except json.JSONDecodeError as e:
-                print(f"Ошибка в JSON: {e}")
-                print("Содержимое файла:")
-                print(my_file.read())    
+        file_extension = file_path.suffix
+        if file_extension in ['.yml', '.yaml']:
+            with open(file_path, 'r') as my_file:
+                try:
+                    my_data = yaml.safe_load(my_file)
+                    return my_data
+                except yaml.YAMLError as e:
+                    print(f"Ошибка в YAML: {e}")
+                    print("Содержимое файла:")
+                    print(my_file.read())
+        elif file_extension == '.json':
+            with open(file_path, 'r') as my_file:
+                try:
+                    my_data = json.load(my_file)
+                    return my_data
+                except json.JSONDecodeError as e:
+                    print(f"Ошибка в JSON: {e}")
+                    print("Содержимое файла:")
+                    print(my_file.read())    
 
     data_1 = read_file(file1)
     data_2 = read_file(file2)
