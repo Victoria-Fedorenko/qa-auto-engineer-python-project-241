59a12eb7e839cb19746eee941de2dfc6caf585d8
Victoria-Fedorenko <vnfedorenko97@gmail.com>
2025-10-09 19:00:04 +0300

    new test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -18,5 +18,6 @@ Property 'verbose' was added with value: true"""
 
 def test_generate_diff():
   assert generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.json') == good_result_stylish
+  assert generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.json', stylish) == good_result_stylish
   assert generate_diff('tests/test_data/filepath1.yml', 'tests/test_data/filepath2.yml') == good_result_stylish
   assert generate_diff('tests/test_data/file1.json', 'tests/test_data/file2.json', plain) == good_result_plain 
